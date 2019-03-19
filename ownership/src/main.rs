cdcb4557af63a58d5a496a8734b73567b367fab3
Patrick <yangsy9@mail2.sysu.edu.cn>
2019-03-19 23:20:15 +0800

    Update main.rs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -4,14 +4,38 @@ fn main() {
     let (s1, len) = calculate_length(s1);
 
     println!("The length of '{}' is {}.", s1, len);
+
+    println!("The length of '{}' is {}.", s1, calculate_length2(&s1));
+
+    println!("I can still use '{}'", s1);
+    
+    //**************************************************/
+    let mut s2 = String::from("hello2");
+
+    {
+        let r1 = &mut s2;
+        change(r1);
+        println!("At inner scope, after change: {}", &(&r1)); // What is this puzzling thing??
+        println!("At inner scope, after change: {}", s2);
+    }
+
+    let r1 = &mut s2;
+    println!("At outer scope, after inner scope change: {}", r1);
+
+    //****************** Slices! **********************/
 }
 
 fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() returns the length of a String
+    let length = s.len();
 
     (s, length)
 }
 
-fn function(immut_ref, $mut_ref, &mut_copy) {
+fn calculate_length2(s: &String) -> usize {
+    s.len()
+}
 
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
